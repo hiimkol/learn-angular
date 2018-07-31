298e82388b289ff6b8bfcee2512b0d522b094e62
Tran Vuong Quang <hiimkol@gmail.com>
2018-07-31 18:17:25 +0700

    almost routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import { Observable, of } from '../../node_modules/rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  constructor(private messageService: MessageService) { }
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
